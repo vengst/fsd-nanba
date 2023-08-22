42867e6416cc9a7c828b2dff0306b54edd5954b5
VENGADESH PANNEERSELVAM <142766159+vengst@users.noreply.github.com>
2023-08-22 20:43:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+// /// Basic of JS language
+
+// ;//line breaker// not necessary
+ console.log("first line of code")
+console.log("second line code")
+ console.log("third line code")
+// //creation of variable
+//varible should be name ..///
+// syntax-> variablekeyword>variableName=value
+ var a=16
+ console.log(a);
+ // variable declaration
+ var b=17
+ console.log(b)
+ //difference typr of varaibles
+//   var let const
+var a=255;//we can reassign and redeclare in var keyword of javascript
+  a=50
+  console.log (a)
+  let abc =25
+  console.log(abc)
+  abc =50  // we can reassign
+  //let abc=50 we can't redeclare
+  console.log(abc); 
+  const studentName='vengat'
+  //console.log(studentName)
+  //studentName=vengat// we can't reassign in const 
+  //const(studentName)=sanjay// we can't redeclare in const 
+  console.log(studentName)// camelcasing 
+  //data types
+   ///primitive
+    // String ==string="" or ''
+    // number or integer numbers
+    // Boolean  true or false
+    // null
+    // undefined
+
+    // non primitive types
+  // array
+  //object
+  const value=null
+  console.log(value)
+ let mani="sanjay"
+ console.log(mani);
+
+ //array
+ let array=["sanjay",true,0,79.65]
+ console.log (array,[]);
+ //objects
+
+//  // key :value
+const package= {nanban:"sanjay",
+   age :25,
+   price :255,}
+
+   
+   console.log(package.lenght)
+///operators
+///loops
+///conditions
+///functions with return keyword
+console.log(a)
